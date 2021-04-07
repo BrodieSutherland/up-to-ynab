bb8dd0438eaef2307c927b0d72ae50dc41573e2c
Brodie Sutherland <brodie.j.sutherland@gmail.com>
2021-04-07 15:39:43 +1000

    trying to make it all work lol

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, Response
 import classes
 import helper
+import os
 
 app = Flask(__name__)
 
-@app.route('/upTransactions', methods=['POST'])
+@app.route('/up_webhook', methods=['POST'])
 def respond():
     eventPayload = classes.UpWebhookEvent(request.json)
     helper.handleWebhookEvent(eventPayload)
@@ -14,4 +15,5 @@ def respond():
 if(__name__ == "__main__"):
     helper.setAllYNABDatabases()
     helper.createUpWebhook()
-    app.run()
+    port = os.environ.get("PORT")
+    app.run(port=port)
