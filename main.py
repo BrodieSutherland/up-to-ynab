28c114d1ac00a44bcf02b59f5ad04a4ce428e654
BrodieSutherland <brodie.j.sutherland@gmail.com>
2023-05-03 12:20:10 +1000

    Use logging framework, output to Gunicorn (#22)

--- main.py
+++ main.py
@@ -1,39 +1,41 @@
-import os
 from flask import Flask, request, Response
 import classes
 import helper
+import os
+import logging
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
+# ---------------
+# ROUTES
+# ---------------
+# Endpoint to handle updates from Up
+@app.route("/", methods=["POST", "GET"])
+def respond() -> Response:
+    if request.method == "POST":
+        """Handles any webhook event from Up"""
+        eventPayload = classes.UpWebhookEvent(request.json["data"])
+        outcome = helper.handleWebhookEvent(eventPayload)
 
-@app.route("/", methods=["POST"])
-def handle_up_webhook() -> Response:
-    """
-    Handles any webhook event from Up.
-    """
-    event_payload = classes.UpWebhookEvent(request.json["data"])
-    outcome = helper.handleWebhookEvent(event_payload)
+        logger.info(outcome)
 
-    print(outcome)
-
-    return Response(status=200)
-
-
-@app.route("/", methods=["GET"])
-def refresh_databases() -> Response:
-    """
-    Refreshes databases.
-    """
-    helper.refresh()
-    return Response(status=200)
-
+        return Response(status=200)
+    elif request.method == "GET":
+        """Refreshes databases"""
+        helper.refresh()
+        return Response(status=200)
 
 if __name__ == "__main__":
-    print("Starting server...")
+    logger.info("Starting server...")
     helper.setAllYNABDatabases()
     if not helper.pingWebhook():
         helper.createUpWebhook()
-    print("Ready for transactions!")
+    logger.info("Ready for transactions!")
     if helper.getEnvs("DEBUG_MODE") == "True":
-        print("DEBUG MODE ENABLED")
+        logger.info("DEBUG MODE ENABLED")
     app.run(host="0.0.0.0", port=os.environ.get("PORT"))
+
